2f072316eafd62a125c5b9e4ab6605f9487a9064
Long Tran <tranvanlong6677@gmail.com>
2023-11-15 13:30:42 +0700

    connect mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://tranvanlong6677:long6677@clusternestjs.l6uizb1.mongodb.net/',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
