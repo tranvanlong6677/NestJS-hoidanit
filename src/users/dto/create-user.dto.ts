d4f1b328498f3b91f008d08b478d7023ce8e3bb0
Long Tran <tranvanlong6677@gmail.com>
2023-11-16 14:36:32 +0700

    validate data with class-validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,23 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsEmail(
+    {},
+    {
+      message: 'Email không đúng định dạng',
+    },
+  )
+  @IsNotEmpty({
+    message: 'Email không được để trống',
+  })
   email: string;
+
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
   password: string;
+
+  @IsString()
   address: string;
 }
